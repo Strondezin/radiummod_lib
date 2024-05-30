2be5f9eef9792f479578f22c3034567fc9a0effa
cristian.makes <121450782+Cristi4n-7@users.noreply.github.com>
2024-05-30 16:41:43 +0100

    Bump version (1.0.2)

--- src/main/java/radium/lib/RadiumLib.java
+++ src/main/java/radium/lib/RadiumLib.java
@@ -45,7 +45,7 @@ import java.util.function.Supplier;
 @Mod(modid = RadiumLib.MODID, version = RadiumLib.VERSION, acceptableRemoteVersions = "*")
 public class RadiumLib {
 	public static final String MODID = "radiumlib";
-	public static final String VERSION = "1.0.1";
+	public static final String VERSION = "1.0.2";
 	public static final SimpleNetworkWrapper PACKET_HANDLER = NetworkRegistry.INSTANCE.newSimpleChannel("radiumlib:a");
 	@SidedProxy(clientSide = "radium.lib.ClientProxyRadiumLib", serverSide = "radium.lib.ServerProxyRadiumLib")
 	public static IProxyRadiumLib proxy;
